b80ae70a2aa545ae342c22b5ba9340634aab8f17
Priscilla <priscilla.srg@gmail.com>
2025-11-10 21:23:39 +0000

    Node desafios - resolucao desafio

--- scripts/scritptMaxNumber.js
+++ scripts/scritptMaxNumber.js
@@ -2,9 +2,28 @@
  * Recebe uma lista de números inteiros e positivos e retorna uma string no formato
  * "{maiorNumero} {indice}" com o maior valor e sua posição.
  */
-function maxNumber(numeros = []) {
-    return "Uuhu"
-}
 
+function maxNumber(numeros = []) {
+  let maiorNumero = 0;
+  let indiceMaiorNumero = 0;
+
+  // -- SOLUÇÃO 1 : FOR
+  //   for (let i = 0; i < numeros.length; i++) {
+  //     if (numeros[i] > maiorNumero) {
+  //       maiorNumero = numeros[i];
+  //       indice = i;
+  //     }
+  //   }
+
+  // -- SOLUÇÃO 2 : FOREACH
+  numeros.forEach((numero, indice) => {
+    if (numero > maiorNumero) {
+      maiorNumero = numero;
+      indiceMaiorNumero = indice;
+    }
+  });
+
+  return `${maiorNumero} ${indiceMaiorNumero}`;
+}
 
 module.exports = { maxNumber };
